1579f3e1d0477d6202f2017d29d82124de6ff3ac
Vagner <vgiraldinojr@gmail.com>
2026-03-05 21:36:50 -0300

    feat: atualiza mensagem de boas-vindas do whatsapp com tom mais leve

--- config/zapi.php
+++ config/zapi.php
@@ -5,7 +5,7 @@ return [
     'token' => env('ZAPI_TOKEN'),
     'client_token' => env('ZAPI_CLIENT_TOKEN'),
     'templates' => [
-        'boas_vindas' => 'Olá {nome}! Recebemos o pagamento da pesquisa (R$ 200,00) do protocolo {protocolo}. Em horário comercial, um analista vai te chamar por aqui com o retorno da análise do CPF/CNPJ.',
+        'boas_vindas' => '👋🏻 Olá {nome}! 👏🏻 Obrigado por fazer parte deste momento com a CPF Clean Brasil. 📍 Recebemos o pagamento da pesquisa (R$ 200,00) do protocolo {protocolo}. 📅 Em horário comercial, um analista vai te chamar por aqui com o retorno da análise do CPF/CNPJ. 🤝🏻 Até breve!',
         'portal_acesso' => 'Olá {nome}! Seu contrato foi confirmado e seu acesso ao portal foi liberado. Link: {link} | Login: {email} | Senha temporária: {senha}. Altere sua senha no primeiro acesso.',
         'status_atualizado' => 'Atualização do seu protocolo {protocolo}: {status}',
         'conclusao' => 'Seu processo de regularização {protocolo} foi concluído! ✅',
